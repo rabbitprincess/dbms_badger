9d6d8c0793d69b537ce5f173cecede9bdda98339
lemon-mint <ice.lemons.mint@gmail.com>
2022-11-02 23:22:42 +0900

    Index Format

---
+++ schema/index.go
@@ -0,0 +1,15 @@
+package schema
+
+/*
+
+# Index Format
+
+key = "_table/<table_id>/<index_id>/<index_key>"
+value = "primary_key"
+
+# Primary Key Format
+
+key = "_table/<table_id>/0/<primary_key>"
+value = "<data>"
+
+*/
