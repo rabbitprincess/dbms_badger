84cbabe08ba99b4e2ab99a86e3ca10e31a27cb2d
lemon-mint <ice.lemons.mint@gmail.com>
2022-11-04 15:06:28 +0900

    Index Key

--- schema/index.go
+++ schema/index.go
@@ -2,14 +2,24 @@ package schema
 
 /*
 
-# Index Format
+# BitSet Index Format
 
-key = "_table/<table_id>/<index_id>/<index_key>
+key = _t/<table_id>/<index_id>/<index_key>
 value = <bitset>
+
+# Unique Index Format
+
+key = _t/<table_id>/<index_id>/<index_key>
+value = <primary_key>
+
+# Range Index Format
+
+key = _t/<table_id>/<index_id>/<index_key>/<primary_key>
+value = <null>
 
 # Primary Key Format
 
-key = "_table/<table_id>/0/<primary_key>"
+key = _t/<table_id>/0/<primary_key>
 value = <data>
 
 */
