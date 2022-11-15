92a9a1aa656cc21c0498e19e7dd2eec13f649168
lemon-mint <ice.lemons.mint@gmail.com>
2022-11-16 00:37:01 +0900

    TODO: Fix Where Query

--- engine/query.go
+++ engine/query.go
@@ -1,1 +1,32 @@
 package engine
+
+type QueryType uint16
+
+const (
+	QueryTypeSelect QueryType = iota
+	QueryTypeInsert
+	QueryTypeUpdate
+	QueryTypeDelete
+
+	QueryTypeExplain
+)
+
+type Query struct {
+	Type QueryType
+}
+
+type Field interface {
+	// Encode appends the binary representation of the field to the byte slice,
+	// and returns the updated slice.
+	Encode(b []byte) ([]byte, error)
+
+	// Decode decodes the binary representation of the field from the byte slice.
+	Decode(b []byte) error
+}
+
+type Where struct {
+	Table    string
+	Column   string
+	Operator string
+	Value    Field
+}
